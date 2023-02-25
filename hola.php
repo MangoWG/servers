72d68be2014167311b670ef8562b8e855a68ecca
MangoWG <125920732+MangoWG@users.noreply.github.com>
2023-02-25 17:13:39 +0100

    primer commit

---
+++ hola.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="static/css/hola.css">
+    <title>hola html</title>
+</head>
+<body>
+    <div class="container">
+        <div class="caja">
+            <div class="nombre">Maria</div>
+            <div class="boton">
+                <input type="button" class="pulsar">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
